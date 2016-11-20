8f017ba5d8ab03a61eeee9e403c25c9268b8711a
DannyBoyNYC <daniel.deverell@gmail.com>
2016-11-20 09:21:26 -0500

    added grunt

--- server.js
+++ server.js
@@ -3,6 +3,41 @@ var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var mongoUri = 'mongodb://localhost/rest-apis';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync')
+
+var sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'expanded'
+};
+
+var sassSources = './assets/scss/**/*.scss';
+var sassOutput = './assets/css';
+var htmlSource = './assets/**/*.html';
+
+var port = process.env.PORT || 3004;
+
+gulp.task('sass', function() {
+    return gulp.src(sassSources)
+        .pipe(sourcemaps.init())
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(sassOutput))
+        .pipe(browserSync.stream())
+});
+
+function listening() {
+    browserSync({
+        proxy: 'localhost:' + port,
+        browser: "google chrome"
+    });
+    gulp.watch(sassSources, ['sass']);
+    gulp.watch(htmlSource).on('change', browserSync.reload);
+}
+
 var db = mongoose.connection;
 mongoose.connect(mongoUri);
 
@@ -17,5 +52,5 @@ db.on('error', function() {
 require('./models/pirate');
 require('./routes')(app);
 
-app.listen(3004);
+app.listen(port, listening);
 console.log('port 3004');
